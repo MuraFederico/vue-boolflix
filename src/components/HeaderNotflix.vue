c26598e10ffa6de1296fb4e6bd2869e8b9a1ffbd
MuraFederico <federicomura98@gmail.com>
2022-03-29 11:02:02 +0200

    added basic styling

--- src/components/HeaderNotflix.vue
+++ src/components/HeaderNotflix.vue
@@ -1,6 +1,10 @@
 <template>
   <header>
-
+      <h1>BOOLFLIX</h1>
+      <nav>
+          <input type="text" placeholder="Search by Name">
+          <button>Cerca</button>
+      </nav>
   </header>
 </template>
 
@@ -10,6 +14,24 @@ export default {
 }
 </script>
 
-<style>
-
-</style>
+<style lang="scss" scoped>
+    header {
+        padding: 1.5rem;
+        display: flex;
+        justify-content: space-between;
+        background-color: black;
+        h1 {
+            color: red;
+        }
+        input {
+            padding: 1rem;
+            margin-right: .5rem;
+        }
+        button {
+            padding: 1rem;
+            background-color: red;
+            border: none;
+            border-radius: .2rem;
+        }
+    }
+</style>>
--- src/components/MainNotflix.vue
+++ src/components/MainNotflix.vue
@@ -16,5 +16,8 @@ export default {
 </script>
 
 <style>
-
+    main {
+        height: 90vh;
+        background-color: darkgray;
+    }
 </style>
